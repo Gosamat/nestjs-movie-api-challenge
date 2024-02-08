98c03ef9627c64c13c2c40c9417c5e91db7f0b5f
Gonçalo Matias <61824985+Gosamat@users.noreply.github.com>
2024-02-08 23:14:32 +0000

    basic swagger ui implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Movie API Coding Challenge')
+    .setDescription(
+      'Welcome to the swagger UI page of my API, in here you can find all about the API endpoints',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
